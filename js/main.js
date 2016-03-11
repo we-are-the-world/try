fb2382f21139b14d2a4184e165890af6f910ca92
guowenfh <guowen921@live.com>
2016-03-11 21:53:29 +0800

    增加配置项

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 (function () {
     $(document).ready(function () {
         $("#fullpage").fullpage({
-            sectionsColor: ["#DE3F3F", "#1795B7", "#E61888", "#21E121", " #8F8484"],
+            // sectionsColor: ["#DE3F3F", "#1795B7", "#E61888", "#21E121", " #8F8484"],
             "verticalCentered": false, //垂直居中
             // "scrollingSpeed":1000,//页面切换速度
             "anchors": ["section1", "section2", "section3", "section4", "section5"], //定义锚链接
-            "paddingTop": "100px", //距离顶部与底部的距离
-            "paddingBottom": "100px",
+            // "paddingTop": "100px", //距离顶部与底部的距离
+            // "paddingBottom": "100px",
             fixedElement: "selector", //fixed的元素
             "navigation": true, //显示导航
             "navigationTooltips": ["徐志鹏", "李言", "韩特", "陈国文", "陈伟钰"], //hover到导航上提示的信息
@@ -15,11 +15,10 @@
             "continuousVertical": true, //连续滚动
             "afterLoad": function (anchorLink, index) {
                 console.log("afterLoad:anchorLink" + anchorLink + ",index:" + index);
-            }
+            },
 
         });
     });
-    $.fn.fullpage.moveTo(4);
 
     var guowen = function () {
 
