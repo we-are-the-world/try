9f774f90ce21f0d13ab2f0d13394c3638c80b692
guowenfh <guowen921@live.com>
2016-04-12 00:13:47 +0800

    -吗m添加拖拽雏形

--- components/task037/main.js
+++ components/task037/main.js
@@ -1,52 +1,99 @@
 /**
  * 事件添加函数(惰性加载)
- * @param { Element  } ele     触发事件的Dom对象
- * @param { String   } event   触发的事件类型
- * @param { Fucntion } func    事件触发执行的函数
+ * @param { HTMLElement  } ele     触发事件的Dom对象
+ * @param { String   } event       触发的事件类型
+ * @param { Fucntion } handler     事件触发执行的函数
  */
 var addEvent = (function() {
     if (document.addEventListener) {
-        return function(ele, event, func) {
-            ele.addEventListener(event, func, false);
+        return function(ele, event, handler) {
+            ele.addEventListener(event, handler, false);
         };
     } else if (document.attachEvent) {
-        return function(ele, event, func) {
-            ele.attachEvent("on" + event, func);
+        return function(ele, event, handler) {
+            ele.attachEvent("on" + event, handler);
         };
     } else {
-        return function(ele, event, func) {
-            ele["on" + event] = func;
+        return function(ele, event, handler) {
+            ele["on" + event] = handler;
         };
     }
 })();
+
+/**
+ * 事件移除函数(惰性加载)
+ * @param { HTMLElement  } ele    需要移除事件的Dom对象
+ * @param { String   } event      移除的事件类型
+ * @param { Fucntion } handler    需要移除的执行函数
+ */
+var removeEvent = (function() {
+    if (document.removeEventListener) {
+        return function(ele, event, handler) {
+            ele.removeEventListener(event, handler, false);
+        };
+    } else if (document.detachEvent) {
+        return function(ele, event, handler) {
+            ele.detachEvent("on" + event, handler);
+        };
+    } else {
+        return function(ele, event, handler) {
+            ele["on" + event] = null;
+        };
+    }
+})();
+
 /**
  * 查找元素中是否具有某个class名
- * @param  {HTMLElement}  element 待查找的html元素
- * @param  {String}  sClass  查找的类名
- * @return {Boolean}         存在为true，否则为false
+ * @param  {HTMLElement}  element 待查找的Dom对象
+ * @param  {String}  sClass       查找的类名
+ * @return {Boolean}              存在为true，否则为false
  */
 function hasClass(element, sClass) {
     if (element && element.className) {
-        return element.className.match(new RegExp("\\b" + sClass + "\\b")) ? true : false;
+        return element.className.match(new RegExp("(\\s|^)" + sClass + "(\\s|$)")) ? true : false;
     } else {
         return false;
     }
 }
 
-(function() {
-    var oBtn = document.getElementById("btn");
-    addEvent(oBtn, "click", function() {
-        var popLayer = document.getElementById("popLayer");
-        poplayer.style.display = "block";
-    });
-})();
+
 
 function popUp(id) {
     var popLayer = document.getElementById(id);
     addEvent(popLayer, "click", function(ev) {
+        console.log(hasClass(ev.target, "poplayer"));
         if (hasClass(ev.target, "poplayer-mask") || hasClass(ev.target, "close-mask")) {
             popLayer.style.display = "none";
         }
     });
 }
-popUp("poplayer");
+
+function setDrag(element) {
+    addEvent(element, "mousedown", onMousedown);
+
+    function onMousedown(ev) {
+        addEvent(document, "mouseup", onMouseup);
+        addEvent(document, "mousemove", onMousemove);
+    }
+
+    function onMousemove(ev) {
+        console.log(ev.clientX);
+    }
+
+    function onMouseup() {
+        removeEvent(document, "mousemove", onMousemove);
+        removeEvent(document, "mouseup", onMouseup);
+    }
+}
+window.onload = function() {
+    (function() {
+        var oBtn = document.getElementById("btn");
+        addEvent(oBtn, "click", function() {
+            var popLayer = document.getElementById("popLayer");
+            poplayer.style.display = "block";
+        });
+    })();
+    popUp("poplayer");
+    var poplayer = document.getElementById("poplayer");
+    setDrag(poplayer);
+};
